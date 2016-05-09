18972af647d6eab34170cc1a7f48608a13b65401
Cole Reynolds <cpjreynolds@gmail.com>
2016-05-09 18:05:15 -0400

    minor changes and adds grid lines display toggle.

--- Main.java
+++ Main.java
@@ -1,18 +1,14 @@
 import javafx.application.Application;
 import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
-import javafx.geometry.Pos;
 import javafx.scene.layout.HBox;
 import javafx.scene.paint.Color;
-import javafx.scene.text.Font;
-import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
@@ -20,7 +16,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage stage) {
-        stage.setTitle("Hello");
+        stage.setTitle("CS Project");
 
         GridPane root = new GridPane();
         root.setAlignment(Pos.CENTER);
@@ -28,37 +24,40 @@ public class Main extends Application {
         root.setVgap(10);
         root.setPadding(new Insets(25, 25, 25, 25));
 
-        Text scenetitle = new Text("Welcome");
-        scenetitle.setFont(Font.font("Tahoma", FontWeight.NORMAL, 20));
-        root.add(scenetitle, 0, 0, 2, 1);
-
-        Label userName = new Label("User Name:");
+        Label userName = new Label("Entry:");
         root.add(userName, 0, 1);
 
-        TextField userTextField = new TextField();
-        root.add(userTextField, 1, 1);
+        TextField textfield = new TextField();
+        root.add(textfield, 1, 1);
 
-        Label pw = new Label("Password:");
-        root.add(pw, 0, 2);
-
-        PasswordField pwBox = new PasswordField();
-        root.add(pwBox, 1, 2);
-
-        Button btn = new Button("Sign in");
-        HBox hbBtn = new HBox(10);
-        hbBtn.setAlignment(Pos.BOTTOM_RIGHT);
-        hbBtn.getChildren().add(btn);
-        root.add(hbBtn, 1, 4);
+        // Submit button.
+        Button submit_btn = new Button("Submit");
+        HBox submit_bound = new HBox(10);
+        submit_bound.setAlignment(Pos.BOTTOM_RIGHT);
+        submit_bound.getChildren().add(submit_btn);
+        root.add(submit_bound, 1, 4);
 
         final Text actiontarget = new Text();
         root.add(actiontarget, 1, 6);
 
-        btn.setOnAction(e -> {
+        submit_btn.setOnAction((ActionEvent e) -> {
             actiontarget.setFill(Color.FIREBRICK);
-            actiontarget.setText("Sign in button pressed");
+            actiontarget.setText("Submit button pressed");
         });
 
-        Scene scene = new Scene(root, 300, 300);
+        // Grid lines toggle.
+        Button gline_btn = new Button("Grid Lines");
+        HBox gline_bound = new HBox(10);
+        gline_bound.setAlignment(Pos.BOTTOM_LEFT);
+        gline_bound.getChildren().add(gline_btn);
+        root.add(gline_bound, 0, 8);
+
+        gline_btn.setOnAction((ActionEvent e) -> {
+            root.setGridLinesVisible(!root.isGridLinesVisible());
+        });
+
+
+        Scene scene = new Scene(root, 300, 250);
         stage.setScene(scene);
         stage.show();
     }
