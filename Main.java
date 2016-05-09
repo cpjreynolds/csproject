2e8bc99ecebceaaa2ddc0396905028e4c09db6db
Cole Reynolds <cpjreynolds@gmail.com>
2016-05-09 11:23:23 -0400

    Form entry

--- Main.java
+++ Main.java
@@ -1,29 +1,64 @@
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.geometry.Pos;
+import javafx.scene.layout.HBox;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class Main extends Application {
+
     @Override
     public void start(Stage stage) {
-        Button btn = new Button();
-        btn.setText("Hello!");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.println("Hello!");
-            }
+        stage.setTitle("Hello");
+
+        GridPane root = new GridPane();
+        root.setAlignment(Pos.CENTER);
+        root.setHgap(10);
+        root.setVgap(10);
+        root.setPadding(new Insets(25, 25, 25, 25));
+
+        Text scenetitle = new Text("Welcome");
+        scenetitle.setFont(Font.font("Tahoma", FontWeight.NORMAL, 20));
+        root.add(scenetitle, 0, 0, 2, 1);
+
+        Label userName = new Label("User Name:");
+        root.add(userName, 0, 1);
+
+        TextField userTextField = new TextField();
+        root.add(userTextField, 1, 1);
+
+        Label pw = new Label("Password:");
+        root.add(pw, 0, 2);
+
+        PasswordField pwBox = new PasswordField();
+        root.add(pwBox, 1, 2);
+
+        Button btn = new Button("Sign in");
+        HBox hbBtn = new HBox(10);
+        hbBtn.setAlignment(Pos.BOTTOM_RIGHT);
+        hbBtn.getChildren().add(btn);
+        root.add(hbBtn, 1, 4);
+
+        final Text actiontarget = new Text();
+        root.add(actiontarget, 1, 6);
+
+        btn.setOnAction(e -> {
+            actiontarget.setFill(Color.FIREBRICK);
+            actiontarget.setText("Sign in button pressed");
         });
 
-        StackPane root = new StackPane();
-        root.getChildren();
-
-        Scene scene = new Scene(root, 300, 250);
-
-        stage.setTitle("Hello");
+        Scene scene = new Scene(root, 300, 300);
         stage.setScene(scene);
         stage.show();
     }
