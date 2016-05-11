b688781dc119a4ea3634acc5dc754462456de4e1
Cole Reynolds <cpjreynolds@gmail.com>
2016-05-11 10:51:03 -0400

    FileChooser

--- Main.java
+++ Main.java
@@ -11,53 +11,40 @@ import javafx.scene.layout.HBox;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
+import javafx.stage.FileChooser;
+
+import java.io.File;
 
 public class Main extends Application {
+    private File target;
 
     @Override
     public void start(Stage stage) {
-        stage.setTitle("CS Project");
+        stage.setTitle("Text Editor");
+        stage.setMinHeight(300);
+        stage.setMinWidth(300);
 
+        // GridPane setup.
         GridPane root = new GridPane();
         root.setAlignment(Pos.CENTER);
         root.setHgap(10);
         root.setVgap(10);
         root.setPadding(new Insets(25, 25, 25, 25));
 
-        Label userName = new Label("Entry:");
-        root.add(userName, 0, 1);
+        // Choose file button.
+        Button choose_btn = new Button("Choose File");
+        HBox choose_bound = new HBox(10);
+        choose_bound.setAlignment(Pos.BOTTOM_RIGHT);
+        choose_bound.getChildren().add(choose_btn);
+        root.add(choose_bound, 0, 0);
 
-        TextField textfield = new TextField();
-        root.add(textfield, 1, 1);
-
-        // Submit button.
-        Button submit_btn = new Button("Submit");
-        HBox submit_bound = new HBox(10);
-        submit_bound.setAlignment(Pos.BOTTOM_RIGHT);
-        submit_bound.getChildren().add(submit_btn);
-        root.add(submit_bound, 1, 4);
-
-        final Text actiontarget = new Text();
-        root.add(actiontarget, 1, 6);
-
-        submit_btn.setOnAction((ActionEvent e) -> {
-            actiontarget.setFill(Color.FIREBRICK);
-            actiontarget.setText("Submit button pressed");
+        choose_btn.setOnAction((ActionEvent e) -> {
+            FileChooser fchooser = new FileChooser();
+            fchooser.setTitle("Choose File");
+            target = fchooser.showOpenDialog(stage);
         });
 
-        // Grid lines toggle.
-        Button gline_btn = new Button("Grid Lines");
-        HBox gline_bound = new HBox(10);
-        gline_bound.setAlignment(Pos.BOTTOM_LEFT);
-        gline_bound.getChildren().add(gline_btn);
-        root.add(gline_bound, 0, 8);
-
-        gline_btn.setOnAction((ActionEvent e) -> {
-            root.setGridLinesVisible(!root.isGridLinesVisible());
-        });
-
-
-        Scene scene = new Scene(root, 300, 250);
+        Scene scene = new Scene(root, 500, 500);
         stage.setScene(scene);
         stage.show();
     }
